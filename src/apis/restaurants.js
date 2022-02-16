fbc95f380b5e6391a18d18a8faa77c3c2f4d9702
Pudding <>
2022-02-16 15:35:30 +0800

    feat: integrate API of getRestaurants in Restaurant View

---
+++ src/apis/restaurants.js
@@ -0,0 +1,12 @@
+import { apiHelper } from '../utils/helpers'
+const getToken = () => localStorage.getItem('token')
+
+export default {
+  getRestaurants({ page, categoryId }){
+    // 產生 queryString
+    const searchParams = new URLSearchParams({ page, categoryId })
+    return apiHelper.get(`restaurants?${searchParams}`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  },
+}
