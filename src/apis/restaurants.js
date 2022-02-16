0b1cee1d02f026533fbf44eaf9be467785497a7c
Pudding <>
2022-02-16 16:35:15 +0800

    feat: integrate API of getFeeds in RestaurantsFeeds View

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -2,11 +2,17 @@ import { apiHelper } from '../utils/helpers'
 const getToken = () => localStorage.getItem('token')
 
 export default {
-  getRestaurants({ page, categoryId }){
+  getRestaurants({ page, categoryId }) {
     // 產生 queryString
     const searchParams = new URLSearchParams({ page, categoryId })
     return apiHelper.get(`restaurants?${searchParams}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
   },
+
+  getFeeds() {
+    return apiHelper.get('restaurants/feeds', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
+  }
 }
