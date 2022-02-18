c281b8c1bf02d179beb446aa423393093157fde5
Pudding <>
2022-02-18 15:35:19 +0800

    feat: integrate getDetail and update admin restaurant API in AdminRestaurantEdit View

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -11,13 +11,26 @@ export default {
   },
 
   restaurants: {
+    getDetail({ restaurantId }) {
+      return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+    },
+
     create({ formData }) {
       return apiHelper.post('/admin/restaurants', formData, {
         headers: { Authorization: `Bearer ${getToken()}` }
       })
     },
+
     get() {
       return apiHelper.get('/admin/restaurants', {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+    },
+
+    update({ restaurantId, formData }) {
+      return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
         headers: { Authorization: `Bearer ${getToken()}` }
       })
     },
--- src/views/AdminRestaurantEdit.vue
+++ src/views/AdminRestaurantEdit.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <AdminRestaurantForm
       :initial-restaurant="restaurant"
+      :is-processing="isProcessing"
       @after-submit="handleAfterSubmit"
     />
   </div>
@@ -9,29 +10,8 @@
 
 <script>
 import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: 'Laurence Reynolds',
-    tel: '1-657-067-3756 x9782',
-    address: '187 Kirlin Squares',
-    opening_hours: '08:00',
-    description: 'sit est mollitia',
-    image:
-      'https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887',
-    viewCounts: 1,
-    createdAt: '2019-07-30T16:24:55.432Z',
-    updatedAt: '2019-07-30T17:26:43.260Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-07-30T16:24:55.429Z',
-      updatedAt: '2019-07-30T16:24:55.429Z'
-    }
-  }
-}
+import adminAPI from '../apis/admin'
+import { Toast } from '../utils/helpers'
 
 export default {
   components: {
@@ -48,43 +28,73 @@ export default {
         description: '',
         image: '',
         openingHours: ''
-      }
+      },
+      isProcessing: false
     }
   },
   created() {
     const { id } = this.$route.params
     this.fetchRestaurant(id)
   },
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next
+  },
   methods: {
-    handleAfterSubmit(formData) {
+    async handleAfterSubmit(formData) {
+      try {
+        const { data } = adminAPI.restaurants.update({
+          restaurantId: this.restaurant,
+          formData
+        })
+
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法更新餐廳資料，請稍後再試'
+        })
+      }
+
       // 透過 API 將表單資料送到伺服器
       for (let [name, value] of formData.entries()) {
         console.log(name + ': ' + value)
       }
     },
-    fetchRestaurant(restaurantId) {
-      console.log('fetchRestaurant id:', restaurantId)
-      const { restaurant } = dummyData
-      const {
-        id,
-        name,
-        CategoryId,
-        tel,
-        address,
-        description,
-        image,
-        opening_hours
-      } = restaurant
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        categoryId: CategoryId,
-        tel,
-        address,
-        description: description,
-        image,
-        openingHours: opening_hours
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await adminAPI.restaurants.getDetail({ restaurantId })
+        const {
+          id,
+          name,
+          CategoryId,
+          tel,
+          address,
+          description,
+          image,
+          opening_hours
+        } = data.restaurant
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          categoryId: CategoryId,
+          tel,
+          address,
+          description: description,
+          image,
+          openingHours: opening_hours
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     }
   }
