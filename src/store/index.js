90e86e5ed0ab7bde8680edb30dac7bc5aaf2856a
Pudding <>
2022-02-21 00:49:40 +0800

    feat: integrate getCurrentUser API in store actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import usersAPI from '../apis/users'
 
 Vue.use(Vuex)
 
@@ -14,17 +15,37 @@ export default new Vuex.Store({
     },
     isAuthenticated: false
   },
-  // 用來修改函式的方法
+  // 用來修改 state 的方法，只允許同步
+  //  commit 發動 mutations
   mutations: {
     setCurrentUser(state, currentUser) {
       state.currentUser = {
         ...state.currentUser,
         // API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
         ...currentUser
-      },
-        state.isAuthenticated = true
+      }
+      state.isAuthenticated = true
     }
   },
-  actions: {},
+  // dispatch 發動 actions
+  actions: {
+    async fetchCurrentUser({commit}) {
+      try {
+        const { data } = await usersAPI.getCUrrentUser()
+        const { id, name, email, image, isAdmin } = data
+
+        commit('setCurrentUser', {
+          id,
+          name,
+          email,
+          image,
+          isAdmin
+        })
+      } catch (error) {
+        console.log('error', error)
+        console.error(error.message)
+      }
+    }
+  },
   modules: {}
 })
