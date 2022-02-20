571ee6b642a6a359fe01e04fa4ddb71c2ac5d847
Pudding <>
2022-02-20 16:39:04 +0800

    feat: set up state、mutations in Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      id: -1,
+      name: '',
+      email: '',
+      image: '',
+      isAdmin: false
+    },
+    isAuthenticated: false
   },
+  // 用來修改函式的方法
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser
+      },
+        state.isAuthenticated = true
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
